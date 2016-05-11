b2ec4b00ac9588d55a875dd60f951dedaf382f37
Dmitry <dmitrybugrov@gmail.com>
2016-05-12 01:54:06 +0300

    update

--- ParseEmailAndGo.go
+++ ParseEmailAndGo.go
@@ -4,7 +4,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
+	"regexp"
 
 	"github.com/DmitryBugrov/log"
 	"github.com/taknb2nch/go-pop3"
@@ -15,11 +17,17 @@ var (
 )
 
 const Config_file = "./config.json"
+const TestFileName = "./mail.dat"
 
 type Cfg struct {
 	Address string
 	User    string
 	Pass    string
+	Rules   []Rule
+}
+type Rule struct {
+	Action  string
+	Subject string
 }
 
 func main() {
@@ -29,17 +37,49 @@ func main() {
 		log.Print(log.LogLevelError, err)
 		panic(0)
 	}
-	fmt.Println(c.Address)
-	ReciveMail()
+	fmt.Println(c.Rules[1].Action)
+	//	ReciveMail()
+	data, err := ReadFromFile(TestFileName)
+	if err == nil {
+		CheckRegExp(data)
+	} else {
+		log.Print(log.LogLevelError, err)
+	}
+}
+
+func WriteToFile(filename string, data string) {
+	ioutil.WriteFile(filename, []byte(data), 0644)
+}
+
+func ReadFromFile(filename string) (string, error) {
+	data, err := ioutil.ReadFile(filename)
+	return string(data), err
+}
+
+func ParsingMail(data string) (string, string) {
+	subject := "-"
+	body := "--"
+	re := regexp.MustCompile("Subject:.*")
+	subject = string(re.Find([]byte(data)))
+
+	re = regexp.MustCompile("(?ms)Content-Type:.*$")
+	body = re.FindString(data)
+	return subject, body
+}
+
+func CheckRegExp(data string) {
+	subject, body := ParsingMail(data)
+	log.Print(log.LogLevelTrace, "subject=", subject, "body=", body)
 }
 
 func ReciveMail() {
 	err := pop3.ReceiveMail(c.Address, c.User, c.Pass,
 		func(number int, uid, data string, err error) (bool, error) {
-			log.Print(log.LogLevelTrace, "%d, %s\n", number, uid)
+			log.Print(log.LogLevelTrace, number, uid)
 
-			log.Print(log.LogLevelWarning, "getting mail...")
-
+			log.Print(log.LogLevelTrace, "getting mail...")
+			WriteToFile(TestFileName, data)
+			CheckRegExp(data)
 			return false, nil
 		})
 	if err != nil {
